c8048cc0b605ee1770c319b179598bf9d86264fd
verybadcoder01 <76253661+verybadcoder01@users.noreply.github.com>
2022-01-03 11:28:39 +0300

    Update main.py
    compressed everything into classes

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class Cell:
             moves = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
         count = 0
         for i in moves:
-            if cells[(self.x + i[0]) % len(cells)][(self.y + i[1]) % len(cells[0])].alive:
+            if field.cells[(self.x + i[0]) % len(field.cells)][(self.y + i[1]) % len(field.cells[0])].alive:
                 count += 1
         return count
 
@@ -32,38 +32,53 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 root = pg.display.set_mode((2000, 2000))
 pg.display.set_caption('Conways game of Life')
-cells = [[Cell(i, j) for j in range(root.get_width() // 20)] for i in range(root.get_height() // 20)]
 
 
-while 1:
-    time.sleep(0.5)
-    # root.fill(WHITE)
-    # for i in range(0, root.get_height() // 20):
-    #     pg.draw.line(root, WHITE, (0, i * 20), (root.get_width(), i * 20))
-    # for j in range(0, root.get_width() // 20):
-    #     pg.draw.line(root, WHITE, (j * 20, 0), (j * 20, root.get_height()))
-    for i in pg.event.get():
-        if i.type == QUIT:
-            quit(0)
-    for i in range(0, len(cells)):
-        for j in range(0, len(cells[i])):
-            print(cells[i][j].alive, i, j)
-            if cells[i][j].alive:
-                pg.draw.rect(root, WHITE, [i * 20, j * 20, 20, 20])
-            else:
-                pg.draw.rect(root, (0, 0, 0), [i * 20, j * 20, 20, 20])
-    pg.display.update()
-    cells2 = [[Cell(_, __, 0) for _ in range(len(cells[0]))] for __ in range(len(cells))]
-    for i in range(len(cells)):
-        for j in range(len(cells[0])):
-            if cells[i][j].alive:
-                if cells[i][j].count_neighbours() not in (2, 3):
-                    cells2[i][j] = Cell(i, j, 0)
+class Field:
+    width = root.get_width() // 20
+    height = root.get_height() // 20
+    cells = [[]]
+
+    def __init__(self, cells_alive=None):
+        self.cells = [[Cell(i, j, cells_alive) for j in range(self.width)] for i in range(self.height)]
+
+    def draw(self):
+        for i in range(0, len(self.cells)):
+            for j in range(0, len(self.cells[i])):
+                if self.cells[i][j].alive:
+                    pg.draw.rect(root, WHITE, [i * 20, j * 20, 20, 20])
+                else:
+                    pg.draw.rect(root, (0, 0, 0), [i * 20, j * 20, 20, 20])
+
+    def update(self):
+        field2 = Field(0)
+        for i in range(len(self.cells)):
+            for j in range(len(self.cells[0])):
+                if self.cells[i][j].alive:
+                    if self.cells[i][j].count_neighbours() not in (2, 3):
+                        field2.cells[i][j] = Cell(i, j, 0)
+                        continue
+                    field2.cells[i][j] = Cell(i, j, 1)
                     continue
-                cells2[i][j] = Cell(i, j, 1)
-                continue
-            if cells[i][j].count_neighbours() == 3:
-                cells2[i][j] = Cell(i, j, 1)
-                continue
-            cells2[i][j] = Cell(i, j, 0)
-    cells = cells2
+                if self.cells[i][j].count_neighbours() == 3:
+                    field2.cells[i][j] = Cell(i, j, 1)
+                    continue
+                field2.cells[i][j] = Cell(i, j, 0)
+        self.cells = field2.cells
+
+
+field = Field()
+
+
+def main():
+    while 1:
+        time.sleep(0.5)
+        for event in pg.event.get():
+            if event.type == QUIT:
+                quit(0)
+        field.draw()
+        pg.display.update()
+        field.update()
+
+
+main()
