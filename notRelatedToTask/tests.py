bcc8c6c77b593f927cac832a9c13fd5cc707d735
verybadcoder01 <76253661+verybadcoder01@users.noreply.github.com>
2022-01-04 18:54:22 +0400

    Update tests.py

--- notRelatedToTask/tests.py
+++ notRelatedToTask/tests.py
@@ -1,5 +1,5 @@
 
-configs = [{}]
+configs = {}
 states = [[[]]]
 
 
@@ -14,18 +14,18 @@ def equals(a, b):
 
 
 def check_evolution(field, iterations: int):
-    if len(states) == 1:
+    if len(states) <= 1:
         return 1
     for i in range(0, len(states)):
         for j in range(0, len(states)):
             if equals(states[i], states[j]) and i != j:
-                configs.append({iterations: field})
+                configs[iterations] = field
                 print("EVOLUTION FINISHED")
                 return 0
     for i in states[-1]:
         for j in i:
             if j.alive == 1:
                 return 1
-    configs.append({iterations: field})
+    configs[iterations] = field
     print("EVOLUTION FINISHED")
     return 0
